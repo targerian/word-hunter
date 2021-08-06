18dc3e67e9237ae05f3fc0ba7f5a0b9a6ed62e48
Cozmo <mohamed@lenovo.local>
2021-08-07 00:39:39 +0200

    1st attempt to web progressive app

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+serviceWorkerRegistration.register();
